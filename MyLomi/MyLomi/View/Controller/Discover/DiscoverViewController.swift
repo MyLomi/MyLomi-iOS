4021e8edeade2e022c02d919d8847fcccd47f4ee
Yohannes <yohannes@Yohaness-MacBook-Air.local>
2023-08-10 19:43:28 +0300

    Prepare Main Tab Bar

---
+++ MyLomi/MyLomi/View/Controller/Discover/DiscoverViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverViewController.swift
+//  MyLomi
+//
+//  Created by Yohannes on 8/10/23.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ MyLomi/MyLomi/View/Controller/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  MyLomi
+//
+//  Created by Yohannes on 8/10/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
